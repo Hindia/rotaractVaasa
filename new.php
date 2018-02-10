ffbaeab4a04a2a3ffa46e4f9e33ee6f6d150d52c
Hindia <mahmmudhindia@gmail.com>
2018-02-10 19:14:31 +0200

    working contact form
    some editing needed for the php style

--- new.php
+++ new.php
@@ -1,25 +1,11 @@
 <?php
-//if "email" variable is filled out, send email
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $comment = $_POST['comment'];
-  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      echo "Invalid email format";
-    }
-  else {
-  
-  //Email information
-  $admin_email = "rotaractvaasa.viestinta@gmail.com";
-  $email_subj = "New Comment on website"
-  $email_body = "$name said :$comment.\n You can reach $name at $email";
-  $to="rotaractvaasa.viestinta@gmail.com";
-  $header="From $admin_email\r\n";
-  //send email
-  mail($to, $email_subj, $email_body,$header);
-  
-  //Email response
-  echo "Thank you for contacting us!";
-  }
-  echo "Failed";
-  
+$name = $_POST['name'];
+$email = $_POST['email'];
+$comment = $_POST['comment'];
+$formcontent=" From: $name \n Email: $email \n Comment: $comment \n";
+$recipient = "rotaractvaasa.viestinta@gmail.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You, We will get back to you ASAP!" . " \n" . "<a href='index.html'>home</a>";
 ?>
